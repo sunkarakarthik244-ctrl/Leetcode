e1d6f0d75e571359fba7db072eb34274cbbcd773
siva karthik <sunkarakarthik244@gmail.com>
2026-08-21 12:19:38 +0530

    Time: 3 ms (99.82%) | Memory: 46.5 MB (25.75%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,5 +1,8 @@
 class Solution {
     public String intToRoman(int num) {
+        //solve Integer to Roman using a Greedy Algorithm.
+
+        
         // Define all Roman symbols including the 6 subtractive edge cases
         int[] values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] symbols={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
