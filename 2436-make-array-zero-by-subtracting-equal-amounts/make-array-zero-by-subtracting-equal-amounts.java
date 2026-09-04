b8a64d443cc0a79fab7bf58867cfd67e25cee088
siva karthik <sunkarakarthik244@gmail.com>
2026-09-05 01:18:44 +0530

    Time: 1 ms (92.50%) | Memory: 43.4 MB (10.98%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -1,22 +1,27 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int count=0;
-        while(true){
-            int min=101;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]!=0){
-                    min=Math.min(min,nums[i]);
-                }
-            }
-            if(min==101) break;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]!=0){
-                    nums[i]=nums[i]-min;
-                }
-            }
-            count++;
+        HashSet<Integer> set=new HashSet<>();
+        for(int ele:nums){
+            if(ele!=0) set.add(ele);
         }
-        return count;
+        return set.size();
+        // int count=0;
+        // while(true){
+        //     int min=101;
+        //     for(int i=0;i<nums.length;i++){
+        //         if(nums[i]!=0){
+        //             min=Math.min(min,nums[i]);
+        //         }
+        //     }
+        //     if(min==101) break;
+        //     for(int i=0;i<nums.length;i++){
+        //         if(nums[i]!=0){
+        //             nums[i]=nums[i]-min;
+        //         }
+        //     }
+        //     count++;
+        // }
+        // return count;
         
         
     }
